ddf88ba3eb683981b23079188671a1640d78cccd
Szymon Stasiak <szymon-stasiak@outlook.com>
2022-06-17 21:19:19 +0200

    Try fixing repeat_cmaes

--- repeat_cmaes.m
+++ repeat_cmaes.m
@@ -16,48 +16,57 @@ function [xmin, fitnessmin, out]=repeat_cmaes(fitness_function, dimensions, repe
   fitnessmin = 2^31;
   out.datx = [];
   out.arx = [];
+  out.elapsed = 0;
   for i = 1:repetitions
-    [temp_xmin, temp_fitnessmin, temp_out] = cmaes(fitness_function, dimensions, extinction_type, seeds(i), lambda);
+    [temp_xmin, temp_fitnessmin, temp_out] = cmaes(fitness_function, dimensions, extinction_type, seeds(i), lambda, extinction_trigger, p_extinction);
+    out.elapsed = out.elapsed + temp_out.elapsed;
     if use_best && temp_fitnessmin < fitnessmin
       xmin = temp_xmin;
       fitnessmin = temp_fitnessmin;
       out = temp_out;
-    else
+    elseif ~use_best
       % Update mean value in xmin
-      xmin = ((i - 1) * xmin + temp_xmin) / i;
+      xmin = xmin + temp_xmin;
       % Update mean value in out.datx
       if height(out.datx) == 0
         out.datx = temp_out.datx;
       elseif height(temp_out.datx) == height(out.datx)
-        out.datx = ((i-1) * out.datx + temp_out.datx) / i;
+        out.datx = out.datx + temp_out.datx;
       elseif height(temp_out.datx) > height(out.datx)
         last = out.datx(end,:);
         diff = height(temp_out.datx) - height(out.datx);
         out.datx = [out.datx; repmat(last, diff , 1)];
-        out.datx = ((i-1) * out.datx + temp_out.datx) / i;
+        out.datx = out.datx + temp_out.datx;
       else
         last = temp_out.datx(end,:);
         diff = height(out.datx) - height(temp_out.datx);
         temp_out.datx = [temp_out.datx; repmat(last, diff , 1)];
-        out.datx = ((i-1) * out.datx + temp_out.datx) / i;
+        out.datx = out.datx + temp_out.datx;
       end
       % Update mean value in out.arx (same as above, but transposed and with different variable)
       if width(out.arx) == 0
         out.arx = temp_out.arx;
       elseif width(temp_out.arx) == width(out.arx)
-        out.arx = ((i-1) * out.arx + temp_out.arx) / i;
+        out.arx = out.arx + temp_out.arx;
       elseif width(temp_out.arx) > width(out.arx)
         last = out.arx(:, end);
         diff = width(temp_out.arx) - width(out.arx);
         out.arx = [out.arx repmat(last, 1, diff)];
-        out.arx = ((i-1) * out.arx + temp_out.arx) / i;
+        out.arx = out.arx + temp_out.arx;
       else
         last = temp_out.arx(:, end);
         diff = width(out.arx) - width(temp_out.arx);
         temp_out.arx = [temp_out.arx repmat(last, 1, diff)];
-        out.arx = ((i-1) * out.arx + temp_out.arx) / i;
+        out.arx = out.arx + temp_out.arx;
       end
     end
   end
-  fitnessmin = fitness_function(xmin);
+
+  out.elapsed = out.elapsed / repetitions;
+  if ~use_best
+    xmin = xmin / repetitions;
+    out.arx = out.arx / repetitions;
+    out.datx = out.datx / repetitions;
+    fitnessmin = fitness_function(xmin);
+  end
 end
