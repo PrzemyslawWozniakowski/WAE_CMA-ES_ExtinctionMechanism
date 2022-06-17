10f190528cd0e630e0c58da8963f13b100e06baf
Szymon Stasiak <szymon-stasiak@outlook.com>
2022-06-17 16:27:24 +0200

    Start extinction params tests

--- test_lambda.m
+++ test_lambda.m
@@ -7,19 +7,19 @@ for lambda = 10:50:410
   disp(['Podstawowy CMA-ES, lambda = ' num2str(lambda)  ', fmin = ' num2str(fitnessmin) ', argmin = ' mat2str(x')]);
   figure(1);
   subplot(3,3,floor(lambda/50) + 1, 'align')
-  hold off; 
+  hold off;
   plot(out.datx);
-  title("Krzywe zbieżności, funkcja Rosenbrocka, lambda = " + lambda); 
+  title("Krzywe zbieżności, funkcja Rosenbrocka, lambda = " + lambda);
   grid on; xlabel('Iteracje'); ylabel('Wartość x_i = argmin_i(f)');
-  
+
   % Empirical cumulative distribution function plot
   figure(2);
   subplot(3,3,floor(lambda/50) + 1)
   hold on;
-  for n = 1:dimensions 
-    ecdf(out.arx(n, :)); 
+  for n = 1:dimensions
+    ecdf(out.arx(n, :));
   end
   hold off;
-  title("Dystrybuanta empiryczna, funkcja Rosenbrocka, lambda = " + lambda); 
+  title("Dystrybuanta empiryczna, funkcja Rosenbrocka, lambda = " + lambda);
   grid on; ylim([0 1.1]); xlabel('x_i'); ylabel('Wartość');
 end
